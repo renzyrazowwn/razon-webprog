8b69480a3c42ee4cc1b2961ecca634ad0398c394
Renzy Razon <renzyrazowwn@gmail.com>
2026-06-06 19:10:48 +0800

    production

--- razon-server/index.js
+++ razon-server/index.js
@@ -12,7 +12,10 @@ connectDB();
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+  );
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   if (req.method === "OPTIONS") {
